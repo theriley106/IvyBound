fc7f76ae8ac2e944db4a21998a656baadc69bc34
theriley106 <christopherlambert106@gmail.com>
2019-05-06 10:49:12 -0400

    Added search

--- main.py
+++ main.py
@@ -7,10 +7,24 @@ def grabSite(url):
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 	return requests.get(url, headers=headers)
 
+def get_page_count(url):
+	res = grabSite(url)
+	page = bs4.BeautifulSoup(res.text, 'lxml')
+	return int(page.select(".LastPage")[0].getText())
+
+class Search(object):
+	"""docstring for Search"""
+	def __init__(self, urlVal):
+		self.main_url = urlVal
+		self.thread = urlVal.partition(".com/")[2].partition("/")[0]
+		print("Searching for {}".format(self.thread))
+		self.pages = get_page_count(self.main_url)
+		print("{} Pages found in the {} thread".format(self.pages, self.thread))
+		
+
 if __name__ == '__main__':
 	#thread = raw_input("College Confidential Thread URL: ")
 	thread = "https://talk.collegeconfidential.com/columbia-school-general-studies/"
 	# IE: https://talk.collegeconfidential.com/columbia-school-general-studies/
-	res = grabSite(thread)
-	page = bs4.BeautifulSoup(res.text, 'lxml')
-	print page.select(".LastPage")[0].getText()
+	
+	cc = Search(thread)
