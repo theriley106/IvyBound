07f263c772cff567eedde2b5d7e4c78d04f046e5
Theriley106 <christopherlambert106@gmail.com>
2019-05-11 22:20:49 -0700

    Updated templates

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ app = Flask(__name__, static_url_path='/static')
 def index():
 	return render_template("index.html")
 
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    projectpath = request.form['projectFilepath']
+    return projectpath
+    # your code
+    # return a response
+
 @app.route('/test', methods=['GET'])
 def testPage():
 	return render_template("index1.html")
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, debug=True)
